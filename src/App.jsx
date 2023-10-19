ce404bc73a48fdbe168f3399e1636d6e0055652e
marco bruno <marco2000bruno@gmail.com>
2023-10-19 13:42:17 +0200

    continuo

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,32 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import CustomButton from './CustomButton'
+
 import { Form } from './Form'
 
 const toDo = [{
-  id:1,
-  title:"titles",
-  completed:false,
+  id: 1,
+  title: "titles",
+  completed: false,
 },
 {
-  id:2,
-  title:"title",
-  completed:true,
+  id: 2,
+  title: "title",
+  completed: true,
 }]
+const defaultValue = { title: "", completed: false, id: null }
 
 function App() {
-  
-  const [todos,setTodos] = useState(toDo)
-  const [selectTodo, setSelectTodo] = useState()
+
+  const [editTodo, setEditTodo] = useState(defaultValue)
+  const [todos, setTodos] = useState(toDo)
+
 
   const handleReset = () => {
     const resetButton = window.document.getElementById("reset");
     resetButton.click();
-}
+  }
 
-  function createNewTodo( event ) {
+  function createNewTodo(event) {
     event.preventDefault();
 
     const data = new FormData(event.target)
@@ -36,38 +38,74 @@ function App() {
       title,
       completed: completed ? true : false,
     }
-    
+
     setTodos(prevList => [...prevList, newListElement]);
     handleReset();
 
-}
-
-const handleRemoveSingleElement = (id) => {
-  
-  setTodos(prevList => prevList.filter(element => element.id !== id))
-}
-
-  return  (
-  <>
-  <Form handleSubmit= {createNewTodo}/>
-  {
-    todos.map((el) => 
-      <div style={{border: '3px solid red'}} key={el.id}>
-        <p style={{backgroundColor: el.completed ? 'green' : 'red'}}> {el.title}</p>
-        <p> {el.id}</p>
-        <p>{el.completed ? "completato": "non completato"}</p>
-        <button id={el.id} onClick={() => handleRemoveSingleElement(el.id)}>remove element</button>
-        <button onClick={() => setSelectTodo(el.id)}>modifica</button>
-        { selectTodo === el.id && 
-        <>
-          <input type="text" value={el.title}/>
-          <input type="checkbox" checked={el.completed}/>
-        </>
-        }
-      </div>
-    )
   }
-  </>)
+
+  const handleRemoveSingleElement = (id) => {
+
+    setTodos(prevList => prevList.filter(element => element.id !== id))
+  }
+
+  const handleEditTodo = (event) => {
+    setEditTodo(prev => {
+      return {
+        ...prev,
+        title: event.target.value
+      }
+    })
+  }
+
+  const handleEditcheck = (event) => {
+    setEditTodo(prev => {
+      return {
+        ...prev,
+        completed: event.target.checked ? true : false
+      }
+    })
+  }
+
+  const handleSaveTodo = () => {
+    setTodos(prev => {
+      console.log(prev);
+      const newTodo = prev.map(el => {
+        if (el.id === editTodo.id) {
+          return {
+            ...el, title: editTodo.title,
+            completed: editTodo.completed
+
+          }
+        } return el
+
+      })
+      return newTodo
+    })
+    setEditTodo(defaultValue)
+  }
+
+  return (
+    <>
+      <Form handleSubmit={createNewTodo} />
+      {
+        todos.map((el) =>
+          <div style={{ border: '3px solid red' }} key={el.id}>
+            <p style={{ backgroundColor: el.completed ? 'green' : 'red' }}> {el.title}</p>
+            <p> {el.id}</p>
+            <p>{el.completed ? "completato" : "non completato"}</p>
+            <button id={el.id} onClick={() => handleRemoveSingleElement(el.id)}>remove element</button>
+            <button onClick={() => { editTodo.id === el.id ? handleSaveTodo() : setEditTodo(el) }}> {editTodo.id === el.id ? "salva" : "modifica"}</button>
+            {editTodo.id === el.id &&
+              <>
+                <input type="text" onChange={handleEditTodo} value={editTodo.title} />
+                <input type="checkbox" onChange={handleEditcheck} checked={editTodo.completed} />
+              </>
+            }
+          </div>
+        )
+      }
+    </>)
 }
 
 export default App
